24dc5d79aa9a6e79c00ee13caef550be3a83e90c
Diego Bardesio <141086093+BardesioDiego@users.noreply.github.com>
2025-10-23 18:09:03 -0300

    commit cambios

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // config.php
 define('SMTP_HOST', 'gtxm1126.siteground.biz'); //
-define('SMTP_USER', 'ventas@alquilerdecontenedor.com'); 
+define('SMTP_USER', 'ventas@atlanticcontainers.com.uy'); 
 define('SMTP_PASS', 'Irigoitia'); 
 define('SMTP_PORT', 465); //
