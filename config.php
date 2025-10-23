5cf2603e51c079e8f385e1b47397fe4b0b0b7224
Diego Bardesio <141086093+BardesioDiego@users.noreply.github.com>
2025-10-23 19:52:53 -0300

    commit cambios

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // config.php
-define('SMTP_HOST', 'mail.atlanticcontainers.com.uy'); 
+define('SMTP_HOST', 'smtp.hostinger.com'); 
 define('SMTP_USER', 'ventas@atlanticcontainers.com.uy'); // <-- NUEVO USUARIO
 define('SMTP_PASS', 'Irigoitia'); // <-- ¡LA CLAVE QUE DEBE FUNCIONAR!
 define('SMTP_PORT', 465); // Puerto estándar (TLS)
